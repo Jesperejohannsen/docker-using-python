f1936490202f5dd8e947491e72c31391cf8f3cac
Jesperejohannsen <you@example.com>
2024-01-19 11:02:42 +0100

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -7,8 +7,13 @@ def main():
     args = sys.argv[4:]
     
     completed_process = subprocess.run([command + args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
-
+    
+    process = subprocess.Popen([command] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    stdout, stderr = process.communicate()
+    sys.stdout.write(stdout.decode("utf-8"))
+    sys.stderr.write(stderr.decode("utf-8")) 
+    
 
 if __name__ == "__main__":
     main()
